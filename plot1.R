e3343b6a543df8db76a1ceac400bda4e826811df
Hanjoo Lim <hjlim@yahoo-inc.com>
2016-01-08 14:44:21 -0800

    update plot1

--- plot1.R
+++ plot1.R
@@ -1,4 +1,4 @@
-epc <- read.table("/data/devel/git/programr/household_power_consumption.txt",header=T,sep=";",colClasses=c("character","character","double","double","double","double","double","double","numeric"),na.strings="?")
+epc <- read.table("household_power_consumption.txt",header=T,sep=";",colClasses=c("character","character","double","double","double","double","double","double","numeric"),na.strings="?")
 epc$DateTime = paste(epc$Date, epc$Time)
 epc$DateTime = as.POSIXlt(epc$DateTime,format="%d/%m/%Y %H:%M:%S")
 epc$Date = NULL
